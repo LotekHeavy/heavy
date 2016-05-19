8e3b172e06f9bb0a016b8eb82afc777b70026c09
Marcel <mail@heavy.ch>
2016-05-19 19:02:26 +0000

    diverses...

---
+++ js/clickheat_parameter.js
@@ -0,0 +1,3 @@
+clickHeatSite = 3;
+clickHeatGroup = encodeURIComponent(window.location.pathname+window.location.search);
+clickHeatServer = 'https://www.heavy.ch/statistic/plugins/ClickHeat/libs/click.php';initClickHeat();
